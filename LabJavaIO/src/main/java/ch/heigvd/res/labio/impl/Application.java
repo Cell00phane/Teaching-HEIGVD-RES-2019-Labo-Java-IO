4891c54f5dbb94065403cb1915c74df8ba6980f1
Stephane Selim <stephan.selim@epfl.ch>
2019-03-05 01:23:51 +0100

    First try on Application

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
@@ -7,10 +7,8 @@ import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
 import ch.heigvd.res.labio.quotes.QuoteClient;
 import ch.heigvd.res.labio.quotes.Quote;
-import java.io.File;
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
+
+import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.io.FileUtils;
@@ -90,6 +88,8 @@ public class Application implements IApplication {
        * one method provided by this class, which is responsible for storing the content of the
        * quote in a text file (and for generating the directories based on the tags).
        */
+      storeQuote(quote, "quotes-" + i + ".utf8");
+
       LOG.info("Received a new joke with " + quote.getTags().size() + " tags.");
       for (String tag : quote.getTags()) {
         LOG.info("> " + tag);
@@ -123,7 +123,25 @@ public class Application implements IApplication {
    * @throws IOException 
    */
   void storeQuote(Quote quote, String filename) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    StringBuilder pathnameBuilder = new StringBuilder(WORKSPACE_DIRECTORY);
+    for(String tag: quote.getTags()) {
+      pathnameBuilder.append("/").append(tag);
+    }
+
+    File file = new File(pathnameBuilder.append("/").append(filename).toString());
+    if(!file.getParentFile().mkdirs()) {
+      throw new IOException("Store quote: Could not create parent directories");
+    }
+
+    if(!file.createNewFile()) {
+      throw new IOException("Create new file: Could not create file");
+    }
+
+    BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+    bos.write(quote.getQuote().getBytes());
+    bos.close();
+
   }
   
   /**
@@ -140,6 +158,12 @@ public class Application implements IApplication {
          * of the the IFileVisitor interface inline. You just have to add the body of the visit method, which should
          * be pretty easy (we want to write the filename, including the path, to the writer passed in argument).
          */
+        try {
+          writer.write(file.getAbsolutePath());
+          writer.close();
+        } catch (IOException e) {
+          System.err.println("Error writing absolute path");
+        }
       }
     });
   }
