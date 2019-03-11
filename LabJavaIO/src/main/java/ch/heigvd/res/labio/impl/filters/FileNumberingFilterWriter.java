4b66307b73c3ffc3ca474e88d304f20b39e061c2
Stephane Selim <stephan.selim@epfl.ch>
2019-03-11 21:41:40 +0100

    implement file numbering

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -1,5 +1,7 @@
 package ch.heigvd.res.labio.impl.filters;
 
+import ch.heigvd.res.labio.impl.Utils;
+
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
@@ -11,7 +13,7 @@ import java.util.logging.Logger;
  * It then sends the line number and a tab character, before resuming the write
  * process.
  *
- * Hello\n\World -> 1\Hello\n2\tWorld
+ * Hello\n\World -> 1\tHello\n2\tWorld
  *
  * @author Olivier Liechti
  */
@@ -23,19 +25,61 @@ public class FileNumberingFilterWriter extends FilterWriter {
     super(out);
   }
 
+  private int lineCount = 1;
+  private boolean startFile = true;
+
+  private boolean foundLineSeparator;
+
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    String filteredString = applyFilterToInput(str.substring(off, off + len));
+
+    super.write(filteredString, 0, filteredString.length());
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    String filteredString = applyFilterToInput(new String(cbuf).substring(off, off + len));
+    super.write(filteredString.toCharArray(), 0, filteredString.length());
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    if(c == '\r') {
+      foundLineSeparator = true;
+    } else if(foundLineSeparator) {
+      if(c == '\n') {
+        // Windows
+        write("\r\n", 0, 2);
+      } else {
+        // Just Mac
+        write("\r" + (char) c, 0, 2);
+      }
+      foundLineSeparator = false;
+    } else {
+      String filteredString = applyFilterToInput(Character.toString((char) c));
+      super.write(filteredString, 0, filteredString.length());
+    }
+  }
+
+  private String applyFilterToInput(String str) {
+    String[] newLineSeparation = Utils.getNextLine(str);
+    StringBuilder sb = new StringBuilder();
+
+    if(startFile) {
+      sb.append(lineCount).append('\t');
+      startFile = false;
+    }
+
+    while(!newLineSeparation[0].isEmpty()) {
+      sb.append(newLineSeparation[0]).append(++lineCount).append('\t');
+      newLineSeparation = Utils.getNextLine(newLineSeparation[1]);
+    }
+
+    // append the rest if trailing string without line separator at the end is found
+    sb.append(newLineSeparation[1]);
+
+    return sb.toString();
   }
 
 }
